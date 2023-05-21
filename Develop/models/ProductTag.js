7024e1dac61d892633fae39ffc9c3fb6c6392177
Aivi <olivianitzel@gmail.com>
2023-05-21 19:08:44 -0400

    edited product.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,14 +6,45 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNUll: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'product',
   }
 );
 
